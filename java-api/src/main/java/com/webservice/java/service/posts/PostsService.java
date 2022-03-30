694b925f14a61faa31111c4e4c13c6ef43bed265
jalhagosipo <gpfla757@naver.com>
2022-03-31 02:19:18 +0900

    feat: use spring data jpa and auditing - java

---
+++ java-api/src/main/java/com/webservice/java/service/posts/PostsService.java
@@ -0,0 +1,41 @@
+package com.webservice.java.service.posts;
+
+
+import com.webservice.java.domain.posts.Posts;
+import com.webservice.java.domain.posts.PostsRepository;
+import com.webservice.java.web.dto.PostsResponseDto;
+import com.webservice.java.web.dto.PostsSaveRequestDto;
+import com.webservice.java.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
