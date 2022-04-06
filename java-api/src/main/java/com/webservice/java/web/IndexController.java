e4f19df8c1e49a834d4446f362c822af99604aa9
jalhagosipo <gpfla757@naver.com>
2022-04-06 22:49:58 +0900

    refactor: use @LoginUser to refactor - java

--- java-api/src/main/java/com/webservice/java/web/IndexController.java
+++ java-api/src/main/java/com/webservice/java/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.webservice.java.web;
 
 
+import com.webservice.java.config.auth.LoginUser;
 import com.webservice.java.config.auth.dto.SessionUser;
 import com.webservice.java.service.posts.PostsService;
 import com.webservice.java.web.dto.PostsResponseDto;
@@ -10,19 +11,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
